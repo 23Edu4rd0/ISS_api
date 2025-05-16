b2f16d7e681b0cf126eeb5a2b14a46b330da609f
23Edu4rd0 <eduardo.vchaves23@gmail.com>
2025-05-16 15:13:26 -0300

    Adaptar main.py para uso no Scheduler do PythonAnywhere

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from utils.email_sender import EmailSender
 import time
 
 if __name__ == "__main__":
-    while True:
-        try:
-            EmailSender().enviar_email()
-        except Exception as e:
-            print(f"Erro ao enviar email: {e}")
-        time.sleep(60)
+    try:
+        EmailSender().enviar_email()
+    except Exception as e:
+        print(f"Erro ao enviar email: {e}")
